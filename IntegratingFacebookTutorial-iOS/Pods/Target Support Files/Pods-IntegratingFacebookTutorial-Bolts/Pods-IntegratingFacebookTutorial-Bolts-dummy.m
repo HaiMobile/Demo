60a3020b1cccb9c7e3b8787ec4f965d5bb2e898e
Dev <hainguyen@Hais-MacBook-Pro.local>
2014-12-23 23:47:39 +0700

    abc
    abcd commit

---
+++ IntegratingFacebookTutorial-iOS/Pods/Target Support Files/Pods-IntegratingFacebookTutorial-Bolts/Pods-IntegratingFacebookTutorial-Bolts-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IntegratingFacebookTutorial_Bolts : NSObject
+@end
+@implementation PodsDummy_Pods_IntegratingFacebookTutorial_Bolts
+@end
---
+++ IntegratingFacebookTutorial-iOS/Pods/Target Support Files/Pods-IntegratingFacebookTutorial-ParseFacebookUtils/Pods-IntegratingFacebookTutorial-ParseFacebookUtils-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IntegratingFacebookTutorial_ParseFacebookUtils : NSObject
+@end
+@implementation PodsDummy_Pods_IntegratingFacebookTutorial_ParseFacebookUtils
+@end
